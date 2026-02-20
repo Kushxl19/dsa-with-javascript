668810b13ea9744dd28fb3e1847faf0f2f91fc90
Kushal <chudasamakushal1@gmail.com>
2026-02-21 01:13:06 +0530

    Implement function to find second largest even number
    Added a function to find the second largest even number in an array.

--- arrays/second-largest.js
+++ arrays/second-largest.js
@@ -17,3 +17,32 @@ function findSecondLargest(arr) {
 }
 
 console.log(findSecondLargest([10,5,20,8]));
+
+//Second largest even number 
+
+function evenLarge(arr) {
+    if (arr.length < 2) {
+        return null;
+    }
+
+    let first = -Infinity
+    let second = -Infinity
+
+    for (let num of arr) {
+
+        if (num % 2 === 0) {
+
+            if (num > first) {
+                second = first;
+                first = num
+            }
+            else if (num > second && num < first) {
+                second = num
+            }
+        }
+
+    }
+    return second === -Infinity ? null : second
+}
+
+console.log(evenLarge([1, 2, 3, 4, 5, 5, 6]))
