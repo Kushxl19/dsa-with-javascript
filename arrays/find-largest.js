04a48d2d7f194bb1e7adfbcb6864e8da2264e606
Kushal <chudasamakushal1@gmail.com>
2026-02-21 00:30:06 +0530

    Added largest and second largest array problems

---
+++ arrays/find-largest.js
@@ -0,0 +1,15 @@
+function findLargest(arr) {
+    if (arr.length === 0) return null;
+
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+console.log(findLargest([23,1,22,100,21,13,20]));
---
+++ arrays/second-largest.js
@@ -0,0 +1,19 @@
+function findSecondLargest(arr) {
+    if (arr.length < 2) return null;
+
+    let first = -Infinity;
+    let second = -Infinity;
+
+    for (let num of arr) {
+        if (num > first) {
+            second = first;
+            first = num;
+        } else if (num > second && num < first) {
+            second = num;
+        }
+    }
+
+    return second === -Infinity ? null : second;
+}
+
+console.log(findSecondLargest([10,5,20,8]));
